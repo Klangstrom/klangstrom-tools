c9352e1f12744a98d726af535b05a76af90bc619
dennisppaul <dennis.paul@hfk-bremen.de>
2025-09-08 15:35:31 +0200

    minor changes

--- update-klangstrom-for-arduino-repositories.sh
+++ update-klangstrom-for-arduino-repositories.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-# run from within `klangstrom-arduino`
+KLST_ARDUINO_REPO=../klangstrom-arduino # run from within `klangstrom-arduino` repository
 
+cd $KLST_ARDUINO_REPO
 git fetch klib
 git subtree pull --prefix=libraries klib main --squash
+git push
