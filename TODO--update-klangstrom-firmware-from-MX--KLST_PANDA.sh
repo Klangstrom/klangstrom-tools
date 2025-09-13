695b073c98134415711ee590260fa5d87ab0e7b7
dennisppaul <dennis.paul@hfk-bremen.de>
2025-09-13 10:52:14 +0200

    added script stubs

---
+++ TODO--update-klangstrom-firmware-from-MX--KLST_PANDA.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# format code with `.clang-format` ( ../klangstrom-hardware/KLST_PANDA/firmware/.clang-format )
+# copy from
+#    ../klangstrom-hardware/KLST_PANDA/firmware/Core/(Inc+Src)
+# to
+#    ../klangstrom-libraries/Klangstrom_KLST_PANDA_STM32_CubeMX/src
---
+++ update-klangstrom-arduino-examples-in-documentation.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# copy all `.ino` files from 
+#
+#     `./klangstrom-libraries/Klangstrom/examples` 
+# to 
+#     `./klangstrom-arduino-docs/_libdoc/code`
+#
